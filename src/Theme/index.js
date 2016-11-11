ff2b381d5a7dd1b3a38de8d05a5f1c402a4e4a31
Nathan Tran <ntran013@gmail.com>
2016-11-11 17:51:42 +1100

    Add shadow

--- src/Theme/index.js
+++ src/Theme/index.js
@@ -3,3 +3,4 @@
 export * as Colors from './Colors';
 export { default as lineWidth } from './lineWidth';
 export { default as radius } from './radius';
+export { default as shadow } from './shadow';
--- stories/index.js
+++ stories/index.js
@@ -84,4 +84,76 @@ storiesOf('Visual Language', module)
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </Text>
     </Text>
+  </View>)
+  .add('Shadow', () => <View
+    style={{
+      flexDirection: 'row',
+      padding: Visual.space(7),
+    }}
+  >
+    <SpacedView
+      style={{
+        width: Visual.space(7),
+        height: Visual.space(7),
+        boxShadow: Visual.shadow(0),
+        ':hover': {
+          boxShadow: Visual.shadow(1),
+        },
+      }}
+    />
+    <SpacedView
+      style={{
+        width: Visual.space(7),
+        height: Visual.space(7),
+        boxShadow: Visual.shadow(1),
+        ':hover': {
+          boxShadow: Visual.shadow(2),
+        },
+      }}
+    />
+    <SpacedView
+      style={{
+        width: Visual.space(7),
+        height: Visual.space(7),
+        boxShadow: Visual.shadow(2),
+        ':hover': {
+          boxShadow: Visual.shadow(3),
+        },
+      }}
+    />
+    <SpacedView
+      style={{
+        width: Visual.space(7),
+        height: Visual.space(7),
+        boxShadow: Visual.shadow(3),
+        ':hover': {
+          boxShadow: Visual.shadow(4),
+        },
+      }}
+    />
+    <SpacedView
+      style={{
+        width: Visual.space(7),
+        height: Visual.space(7),
+        boxShadow: Visual.shadow(4),
+        ':hover': {
+          boxShadow: Visual.shadow(1),
+        },
+      }}
+    />
   </View>);
+
+// Helpers
+
+const SpacedView = ({ style, ...props }) => <View
+  {...props}
+  style={{
+    marginLeft: Visual.space(5),
+    ':first-child': {
+      marginLeft: 0,
+    },
+    borderRadius: Visual.radius(2),
+    transition: 'all 200ms cubic-bezier(0.4, 0, 0.2, 1)',
+    ...style,
+  }}
+/>;
